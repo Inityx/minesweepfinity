ebcffb77788832a69a83337dbde472d99909006d
Andrew Tolvstad <CTGBandy@gmail.com>
2015-09-15 05:25:12 -0700

    Started chunk creation implementation

--- src/msgame.rs
+++ src/msgame.rs
@@ -1,16 +1,46 @@
 #![allow(dead_code)]
 extern crate rand;
+use std::collections::HashMap;
+
+const MIN_M: u8 = 16;
+const MAX_M: u8 = 64;
 
 pub struct World {
-	size: u64,
+	allocated: u64,
+	activated: u64,
+	board: HashMap<(i32,i32),Chunk>,
 }
 
 impl World {
-	// enmine();
-	// calc_neighbors()
+	pub fn new() -> World {
+		World {
+			allocated: 0,
+			activated: 0,
+			board: HashMap::new(),
+		}
+	}
+	
+	fn chunk_create(&mut self, x: i32, y: i32) {
+		// TODO: add current chunk, surrounding chunks if not exist
+		//		 calculate neighbors for current chunk
+		//		 increment size
+	}
+	
+	fn chunk_add(&mut self, x: i32, y: i32) {
+		self.board.insert((x,y), Chunk::new());
+	}
+
+	fn calc_neighbors(&mut self) {}
+}
+
+enum ChunkStat {
+	Mined,
+	Neighbored,
+	Won,
 }
 
 pub struct Chunk {
+	stat: ChunkStat, // status
 	mines: [u16;16], // mines
 	vis: [u16;16], // visibility
 	nhb: [u64;16], // neighbors
@@ -18,10 +48,24 @@ pub struct Chunk {
 
 impl Chunk {
 	pub fn new() -> Chunk {
-		Chunk {
+		let mut c = Chunk {
+			stat: ChunkStat::Mined,
 			mines: [0;16],
 			vis: [0;16],
 			nhb: [0;16],
+		};
+
+		for _ in 1..(rand::random::<u8>()%(MAX_M-MIN_M)+MIN_M) {
+			// duplicate entries are not of consequence.
+			c.enmine(rand::random::<u8>()%16, rand::random::<u8>()%16);
+		}
+		
+		return c;
+	}
+	
+	fn enmine(&mut self, row: u8, col: u8) {
+		if (row < 15) & (col < 15) {
+			self.mines[row as usize] = self.mines[row as usize] | (1u16 << (15-col));
 		}
 	}
 	
@@ -38,30 +82,3 @@ impl Chunk {
 		else { false }
 	}
 }
-
-
-#[test]
-fn test_chunk_construct() {
-	let c = Chunk::new();
-	for i in 0..15 { assert_eq!(c.mines[i], 0); }
-	for i in 0..15 { assert_eq!(c.vis[i], 0); }
-	for i in 0..15 { assert_eq!(c.nhb[i], 0); }
-}
-
-#[test]
-fn test_chunk_click() {
-	let mut c = Chunk::new();
-	c.click(0, 2);
-	assert_eq!(c.vis[0], 8192);
-}
-
-#[test]
-fn test_chunk_is_clicked() {
-	let mut vec = Vec::new();
-	let mut c = Chunk::new();
-	for _ in 0..15 { vec.push((rand::random::<u8>()%15, rand::random::<u8>()%15)); }
-	for i in &vec { c.click(i.0, i.1); };
-	for i in &vec { assert!(c.is_clicked(i.0, i.1)); }
-}
-
-
