1b5398b58338b0edbd35a43641501a4e70d067eb
Andrew Tolvstad <CTGBandy@gmail.com>
2015-09-16 01:47:20 -0700

    Added ncurses

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-//extern crate ncurses;
-//use ncurses::*;
+extern crate ncurses;
+use ncurses::*;
 
 mod msgame;
 
+#[allow(unused_variables)]
+#[allow(unused_mut)]
 fn main() {
 	let mut w = msgame::World::new();
 	w.touch(0,0);
+
+	initscr();
+	printw("Hello, world");
+	refresh();
+	getch();
+
+	endwin();
 }
 
