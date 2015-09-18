02e18cb9312052077ebc36fb5ddfc199c1b503e8
Andrew Tolvstad <CTGBandy@gmail.com>
2015-09-17 18:27:06 -0700

    Format and organization

--- src/msgame.rs
+++ src/msgame.rs
@@ -44,7 +44,7 @@ impl World {
 		for i in 0..16 { for j in 0..16 {
 			v.push(
 				if c.is_clicked(i,j) {
-					SquareView::Clicked(c.get_nhb(i,j))
+					SquareView::Clicked(c.get_neighbors(i,j))
 				} else {
 					SquareView::Unclicked {
 						mine: show_mines && c.is_mine(i,j),
@@ -84,7 +84,7 @@ struct Chunk {
 	mines: [u16;16],    // mines
 	vis: [u16;16],      // visibility
 	flags: [u16;16],    // flags
-	nhb: [u64;16],      // neighbors
+	neighbors: [u64;16],// neighbors
 }
 
 impl Chunk {
@@ -94,7 +94,7 @@ impl Chunk {
 			mines: [0;16],
 			vis: [0;16],
 			flags: [0;16],
-			nhb: [0;16],
+			neighbors: [0;16],
 		};
 		for _ in 1..(rand::random::<u8>()%(MMAX - MMIN) + MMIN+ 1) {
 			// duplicate entries are not of consequence.
@@ -103,34 +103,38 @@ impl Chunk {
 		return c;
 	}
 	
-	// TODO refactor so binary grids have one accessor with enum field
 	fn enmine(&mut self, row: u8, col: u8) { debug_assert!(row<15 && col<15);
 		self.mines[row as usize] |= 1u16<<(15-col);
+	}
+	
+	fn click (&mut self, row: u8, col: u8) { debug_assert!(row<15 && col<15);
+		self.vis  [row as usize] |= 1u16<<(15-col);
+	}
+
+	fn enflag (&mut self, row: u8, col: u8) { debug_assert!(row<15 && col<15);
+		self.flags[row as usize] |= 1u16<<(15-col);
+	}
+
+	fn set_neighbors(&mut self, row: u8, col: u8, n: u8) { debug_assert!(row<15 && col<15);
+		self.neighbors[row as usize] = (self.neighbors[row as usize] & !(15u64<<((15-col)*4))) | (n as u64) << ((15-col)*4);
 	}
 	
 	fn is_mine (&self, row: u8, col: u8) -> bool { debug_assert!(row<15 && col<15);
 		self.mines[row as usize] & 1u16<<(15-col) == 1u16<<(15-col)
 	}
 
-	fn click (&mut self, row: u8, col: u8) { debug_assert!(row<15 && col<15);
-		self.vis[row as usize] |= 1u16<<(15-col);
-	}
-
 	fn is_clicked (&self, row: u8, col: u8) -> bool { debug_assert!(row<15 && col<15);
-		self.vis[row as usize] & 1u16<<(15-col) == 1u16<<(15-col)
+		self.vis  [row as usize] & 1u16<<(15-col) == 1u16<<(15-col)
 	}
 	
-	fn enflag (&mut self, row: u8, col: u8) { debug_assert!(row<15 && col<15);
-		self.flags[row as usize] |= 1u16<<(15-col);
-	}
-
 	fn is_flag (&self, row: u8, col: u8) -> bool { debug_assert!(row<15 && col<15);
 		self.flags[row as usize] & 1u16<<(15-col) == 1u16<<(15-col)
 	}
 
-	fn get_nhb(&self, row: u8, col: u8) -> u8 { debug_assert!(row<15 && col<15);
-		// self.nhb[row as usize] 
-		3
+	fn get_neighbors(&self, row: u8, col: u8) -> u8 { debug_assert!(row<15 && col<15);
+		((self.neighbors[row as usize] & 15u64<<((15-col)*4))>>((15-col)*4)) as u8
 	}
 }
 
+#[test]
+fn test_neighbors_accessors() {}
