c23cd0b1322b226152a594d89a6600fc00d82fb3
Andrew <ctgbandy@gmail.com>
2016-04-10 11:07:46 +0000

    Minorly restructure main function

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     let mut g = msgame::Game::new();
     g.test_touch();
     
-    if !env::args().any(|x| x == "--noprint") {
+    if env::args().any(|x| x == "--noprint") {
+        g.chunk_debug();
+    } else {
         g.init_printer();
         g.print();
         ncurses::getch();
-    } else {
-        g.chunk_debug();
     }
 }
 
